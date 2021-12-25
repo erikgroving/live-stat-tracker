2a9d49e44e0e996cb14c72d5cf6c5b545a022814
Erik Meade <egmeade@live.com>
2021-12-25 04:01:42 -0800

    prototype

--- stat-tracker/src/App.js
+++ stat-tracker/src/App.js
@@ -1,6 +1,7 @@
-import Games from "./modules/Games";
+//import Games from "./modules/Games";
 import PlayerTracker from "./modules/PlayerTracker";
 import Grid from '@mui/material/Grid';
+import axios from 'axios';
 
 import { useState } from 'react';
 
@@ -10,7 +11,7 @@ function createNbaDateString() {
 	today = new Date(today + offset);
 	let year = today.getFullYear();
 	let month = today.getMonth() + 1;
-	let day = 23;//today.getDate();
+	let day = today.getDate();
 	let date = year.toFixed() + month.toFixed() + day.toFixed();
 
     return date;
@@ -20,25 +21,29 @@ function fetchGames(setGames) {
     let date = createNbaDateString();
 	const url = `https://data.nba.net/prod/v1/${date}/scoreboard.json`;
 	
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+    axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
         .then(response => {
-            if (response.ok) {
-                return response.json();
+            if (response.status === 200) {
+                return response.data.contents;
             }
             
             throw new Error('Network response was not ok.')
         })
         .then(data => {
-            let parsed = JSON.parse(data.contents);
-            let games = parsed.games.map(g => {
-                return {
-                    gameId: g.gameId,
-                    home: g.hTeam,
-                    away: g.vTeam
-                };
-            });
+            let parsed = JSON.parse(data);
+            if (parsed.games !== undefined) {
 
-            setGames(games);
+
+                let games = parsed.games.map(g => {
+                    return {
+                        gameId: g.gameId,
+                        home: g.hTeam,
+                        away: g.vTeam
+                    };
+                });
+                setGames(games);
+            }
+
         });
     
 }
@@ -49,16 +54,18 @@ async function fetchPlayerStats(games, setPlayerStats) {
     for (let i = 0; i < games.length; i += 1) {
         let g = games[i];
         const url = `https://data.nba.net/prod/v1/${date}/${g.gameId}_boxscore.json`;
-        await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+        console.log(url);
+        
+        await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
             .then(response => {
-                if (response.ok) {
-                    return response.json();
+                if (response.status === 200) {
+                    return response.data.contents;
                 }
                 
                 throw new Error('Network response was not ok.')
             })
             .then(data => {
-                let parsed = JSON.parse(data.contents);
+                let parsed = JSON.parse(data);
                 if (parsed.stats === undefined) {
                     return;
                 }
@@ -67,6 +74,7 @@ async function fetchPlayerStats(games, setPlayerStats) {
                 playerStats.push(players);
 
             })
+            
     }
     setPlayerStats(playerStats);
 }
@@ -87,7 +95,7 @@ function App() {
 
     // Get players in each game and their stats
     if (games.length !== 0 && playerStats.length === 0) {
-        fetchPlayerStats(games, setPlayerStats);
+        setInterval(() => fetchPlayerStats(games, setPlayerStats), 2000);
     }
     
     let pDict = {};
@@ -103,7 +111,7 @@ function App() {
 
     return (
         <Grid container>
-            <Games games={games}/>
+            {/*<Games games={games}/>*/}
             <PlayerTracker playerDict={playerDict}/>
         </Grid>
     );
--- stat-tracker/src/modules/PlayerTracker.js
+++ stat-tracker/src/modules/PlayerTracker.js
@@ -1,16 +1,15 @@
 import React from 'react';
-//import { useState } from 'react';
-/*import Table from '@mui/material/Table';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Grid from '@mui/material/Grid';
+import { useState } from 'react';
+import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';*/
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 
 
 function isEmptyObject(obj) {
@@ -19,9 +18,12 @@ function isEmptyObject(obj) {
 
 export default function PlayerTracker( {playerDict} ) {
 
+
+    const [trackedPlayers, setTrackedPlayers] = useState([]);
+
 	if (isEmptyObject(playerDict)) {
 		return (
-			<p>Loading player stats...</p>
+			<Grid container justifyContent="center">Loading player stats... (Ensure games have started)</Grid>
 		)
 	}
 
@@ -35,26 +37,87 @@ export default function PlayerTracker( {playerDict} ) {
 		});
 	}
 
-	players.sort((a, b) => a.label < b.label ? -1 : 1);
+	const duration = 300;
 
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered:  { opacity: 1 },
+  exiting:  { opacity: 0 },
+  exited:  { opacity: 0 },
+};
+
+	players.sort((a, b) => a.label < b.label ? -1 : 1);
+	console.log(trackedPlayers);
 	return (
-		<Grid container>
+		<>
+		<Grid container item justifyContent="center">
+			<h1>Live Stat Tracker</h1>
+		</Grid>
+		<Grid container spacing={2} justifyContent="center" style={{paddingTop: '50px'}}>
 			<Grid item>
 				<Autocomplete
 					blurOnSelect
 					options={players}
 					sx={{ width: 300 }}
 					renderInput={(params) => <TextField {...params} label="Search for Player" variant="standard"/>}
-					onChange={ (event, inputVal) => {
+					onChange={ (_, inputVal) => {
 						console.log(inputVal);
+						console.log(inputVal.stats);
+						setTrackedPlayers([...trackedPlayers, inputVal]);
 					}}
 					isOptionEqualToValue={(a, b) => a.label === b.label}
 				/>
 			</Grid>
-			<Grid item>
-				<Button variant="outlined">Track Player</Button>
-			</Grid>
 			
+			<Grid container justifyContent="center" item md={12}>
+
+				{
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 650 }} aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Name</TableCell>
+									<TableCell>Points</TableCell>
+									<TableCell>Assists</TableCell>
+									<TableCell>Rebounds</TableCell>
+									<TableCell>FG/M</TableCell>
+									<TableCell>FG/A</TableCell>
+									<TableCell>3P/M</TableCell>
+									<TableCell>3P/A</TableCell>
+									<TableCell>Blocks</TableCell>
+									<TableCell>Steals</TableCell>
+									<TableCell>Turnovers</TableCell>
+								</TableRow>
+							</TableHead>
+							
+							<TableBody>
+								{trackedPlayers.map(player =>
+									<TableRow key={player.label}>
+										<TableCell>{player.label}</TableCell>
+										<TableCell>{player.stats.points}</TableCell>
+										<TableCell>{player.stats.assists}</TableCell>
+										<TableCell>{player.stats.totReb}</TableCell>
+										<TableCell>{player.stats.fgm}</TableCell>
+										<TableCell>{player.stats.fga}</TableCell>
+										<TableCell>{player.stats.tpm}</TableCell>
+										<TableCell>{player.stats.tpa}</TableCell>
+										<TableCell>{player.stats.blocks}</TableCell>
+										<TableCell>{player.stats.steals}</TableCell>
+										<TableCell>{player.stats.turnovers}</TableCell>
+									</TableRow>
+								)}
+							</TableBody>
+						</Table>
+					</TableContainer>
+
+				}
+			</Grid>
 		</Grid>
+		</>
 	)
 }
